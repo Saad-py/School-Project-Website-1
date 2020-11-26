d3df1333f3766324fbb41094e6f16dc16ee3f16f
Saad-py <73306005+Saad-py@users.noreply.github.com>
2020-11-26 21:36:01 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var intro_para = "The UAE is a country in the Middles East or South-East Asia. It is well known for its skyscrapers and tourism.\nThe UAE was formed on the 2nd of December 1971 by Sheikh Zayed bin Sultan Al-Nahyan.\n In the present, Sheikh Khalifa bin Zayed al-Nahyan is the prsident of the UAE and the ruler of Abu Dhabi.\nThe UAE has 7 emirates (states) and they are Ajman, Sharjah, Dubai, Abu Dhabi, Umm-Al-Quwain, Fujairah, Ras-Al-Khaimah. The most famous tourist points in the UAE are:\n 1. Burj Khalifa\n2. Sheikh Zayed Mosque\n 3. Burj Al-Arab\n 3. Hajar Mountains\n 4. Louvre Abu Dhabi\n 5. Jebel Jais\n 6. Sharjah Musuem of Islamic Civilization\n 7. Jebel Jais. \n"
+
+document.getElementById('intro_para').innerHTML = intro_para
+
+var center_para = "Emirati culture is based on Arabian culture, with influences from the cultures of East Africa and Indian Subcontinent.[4] The religion of Islam has had a prominent influence on local architecture, music, attire, cuisine, and lifestyle. Five times every day, Muslims are called to prayer from the minarets of mosques, which are scattered around the country.\n The weekend begins on Friday due to Friday being the holiest day for Muslims; most Muslim countries have a Friday-Saturday or Thursday-Friday weekend. The city of Al Ain in Abu Dhabi is a UNESCO World Heritage site. The Emirate of Sharjah was named The Cultural capital of the Arab World  by UNESCO in 1998 and the Capital of Islamic Culture for 2014 by the OIC."
+
+document.getElementById('center_para').innerHTML = center_para
+
